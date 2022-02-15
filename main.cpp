d81d2921474f1899be3ac9d85e5bb6605568deab
Quang Pham <quang.pham@utah.edu>
2022-02-14 21:46:48 -0700

    Remove redundant output messages

--- main.cpp
+++ main.cpp
@@ -5,12 +5,10 @@
 int main(int argc, char **argv) {
     try {
         use_arguments(argc, argv);
-        std::cout << "Program exiting. Exit code 0\n";
         return 0;
     }
     catch (std::runtime_error exn) {
         std::cerr << "Error: " << exn.what() << "\n";
-        std::cout << "Program exiting with an error. Exit code 1\n";
         return 1;
     }
 }
